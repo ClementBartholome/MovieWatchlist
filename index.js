f2adc867a7e432baaf6733fde5a070a7a967c36b
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-05-04 10:45:35 +0200

    Add button white bg

--- index.js
+++ index.js
@@ -53,10 +53,6 @@ function addMovieToWatchlist(movieId) {
   <img class="remove-icon" data-id="${movieId}" src="images/Remove.png">
   Remove
 `;
-  /*
-  const removeIcon = watchlistBtn.querySelector(".remove-icon");
-  removeIcon.classList.remove("hidden");
-  */
 }
 
 function removeMovie(movieId) {
